bec19465589ee09905f5e726acc970b889f9036d
Kevin <32711005+Genecis@users.noreply.github.com>
2021-01-03 12:09:25 -0500

    cleaned up for deployment

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+/********************************************************************/
+/****************module imports and setup****************************/
+/********************************************************************/
+
 const express = require("express");
 const UUID = require("uuid");
 const bodyParser = require("body-parser");
-const debug = false;
 const app = express();
 const crypto = require("crypto");
 
@@ -14,105 +17,120 @@ app.use(express.static("public"));
 
 let gamePort = process.env.PORT || 3000;
 
-function Room(host){
+const debug = false;
+
+//global to keep track or existing rooms on the server
+let rooms = [];
+
+//room structure for consistency
+function Room(host) {
   this.hostID = host;
   this.players = new Array();
 }
 
-let rooms = [];
+/********************************************************************/
+/**************************get routes********************************/
+/********************************************************************/
 
-//By default, we forward the / path to index.html automatically.
+//root route to join/create page
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/home.html");
 });
 
+//lobby parameters for host vs join and optional join ID to be processed locally
+//joinID is cut out and emitted to the socket for room updates
 app.get("/lobby/:type/:joinID?", function (req, res) {
   res.sendFile(__dirname + "/views/game.html");
 });
 
-app.get("/test", function (req, res) {
-  res.send("text string");
-});
+/********************************************************************/
+/*************************post routes********************************/
+/********************************************************************/
 
 app.post("/host", function (req, res) {
   res.redirect("/lobby/host");
 });
 
 app.post("/join", function (req, res) {
-  console.log(req.body);
   let requestedID = req.body.game_id;
-  console.log(requestedID);
+  console.log("requested to join: " + requestedID);
   res.redirect("/lobby/join" + requestedID);
 });
 
+//posted to by client when setting up a games - pass data to other sockets in the same room to match
+//update players must be sent after setup so the the number of active game players is determined
 app.post("/setup", function (req, res) {
   io.to(req.body.hostID).emit("call_setup", req.body);
-  for(let i = 0; i < rooms.length; i++){
-    if(rooms[i].hostID === req.body.hostID){
+  for (let i = 0; i < rooms.length; i++) {
+    if (rooms[i].hostID === req.body.hostID) {
       io.to(req.body.hostID).emit("update_players", rooms[i].players);
     }
   }
+  console.log("setup game: ")
   console.log(req.body);
   res.sendStatus(200);
 });
 
+//posted to by client when a move is made and game data needs to pass to other sockets in the same room
 app.post("/update", function (req, res) {
   io.to(req.body.hostID).emit("call_update", req.body);
+  console.log("update game: ")
   console.log(req.body);
   res.sendStatus(200);
 });
+
+/********************************************************************/
+/************************Socket setup********************************/
+/********************************************************************/
 
 //Create a socket.io instance using our express server
 io.on("connection", (socket) => {
   console.log("a user is connected");
 
   //on connection give the user both a player id and host id
-  let ids = {player: UUID.v4(), host: crypto.randomBytes(2).toString('hex')};
-  socket.emit("on_connect", {player: ids.player, host: ids.host});
+  let ids = { player: UUID.v4(), host: crypto.randomBytes(2).toString("hex") };
+  socket.emit("on_connect", { player: ids.player, host: ids.host });
   console.log("assigned uuid: " + ids.player + " | game id: " + ids.host);
 
   //create a room on setup
   socket.on("host_setup", (id) => {
-    console.log(id);
     socket.join(id.host);
     let r = new Room(id.host);
-    r.players.push(id.player)
+    r.players.push(id.player);
     rooms.push(r);
     console.log("hosted: " + id.host);
-    console.log(rooms);
   });
 
   //join existing room or create new
   socket.on("join_lobby", (id) => {
-    console.log(id);
     socket.join(id.join);
     console.log("joined: " + id.join);
-    for(let i = 0; i < rooms.length; i++){
-      if(rooms[i].hostID === id.join){
+    for (let i = 0; i < rooms.length; i++) {
+      if (rooms[i].hostID === id.join) {
         socket.join(id.join);
         rooms[i].players.push(id.player);
         console.log(rooms);
         io.to(id.join).emit("player_joined", rooms[i].players.length);
         break;
-      }
-      else if( i === (rooms.length - 1)){
+      } else if (i === rooms.length - 1) {
         //no match found by end of rooms search
         console.log("room not found");
         io.to(id.join).emit("send_home");
       }
     }
-  
-    socket.on('disconnecting', () => {
-      console.log(socket.rooms); // the Set contains at least the socket ID
-      if(socket.rooms.size > 1){
-        let myArr = Array.from(socket.rooms)
+
+    socket.on("disconnecting", () => {
+      if (socket.rooms.size > 1) {
+        let myArr = Array.from(socket.rooms);
         io.to(myArr[1]).emit("player_left");
       }
     });
-
   });
-
 });
+
+/********************************************************************/
+/**************************Open Server*******************************/
+/********************************************************************/
 
 const server = http.listen(gamePort, () => {
   console.log("server is running on port", server.address().port);
--- public/client.js
+++ public/client.js
@@ -1,5 +1,6 @@
-let debug = true;
+let debug = false;
 
+//script variables that display and govern game state
 var canvas = document.getElementById("viewport");
 var context = canvas.getContext("2d");
 var gameData = {
@@ -11,7 +12,7 @@ var gameData = {
   lastMove: [0, 0],
   hostID: "",
   playerID: "",
-  inLobby: 1
+  inLobby: 1,
 };
 
 function Player(uuid, id, colour, score) {
@@ -21,8 +22,10 @@ function Player(uuid, id, colour, score) {
   this.score = score;
 }
 
+//connect to socket IO instance on the server - socket IO script files must be loaded in HTML as well
 var socket = io();
 
+//socket based functions that the server will initiate
 socket.on("call_setup", remoteSetupGame);
 socket.on("call_update", remoteUpdateGame);
 socket.on("on_connect", handleConnection);
@@ -31,118 +34,13 @@ socket.on("player_joined", playerJoined);
 socket.on("player_left", playerLeft);
 socket.on("send_home", sendHome);
 
-function playerJoined(count){
-  gameData.inLobby = count;
-  $("#player-count").html(gameData.inLobby + " players in lobby");
-}
+/********************************************************************/
+/*******************socket processing functions**********************/
+/********************************************************************/
 
-function playerLeft(count){
-  gameData.inLobby--;
-  $("#player-count").html(gameData.inLobby + " players in lobby");
-}
-
-function updatePlayers(idArr){
-  console.log(idArr);
-  gameData.inLobby = idArr.length;
-  let thisPlayer = "";
-  if(gameData.players.length > 0 ){
-    //account for players in lobby being either more of less than players in the game
-    //for more in lobby - assign in order of joining
-    //for more in game - repeat assigns
-    let k = 0;
-    for(let i = 0; i < gameData.players.length; i++){
-      gameData.players[i].uuid = idArr[k];
-      k++;
-      if(k >= idArr.length) k = 0;
-      if(gameData.players[i].uuid === gameData.playerID){
-        if(thisPlayer.length >= 1) thisPlayer += ",";
-        thisPlayer += gameData.players[i].id;
-      }
-    }
-  }
-  $("#player-num").html("Player: " + thisPlayer);
-}
-
-function sendHome(){
-  window.location.href = window.location.origin;
-}
-
-//assign player client IDs to player objects for turns
-function handleConnection(ids) {
-  if(debug)console.log(ids);
-  gameData.hostID = ids.host;
-  gameData.playerID = ids.player;
-
-  //url is lobby/host or lobby/join/gameID depending on previous selection
-  let path = window.location.pathname;
-  if(path.includes("host")){
-    let setupID = {host: gameData.hostID, player: gameData.playerID};
-    socket.emit("host_setup", setupID);
-    //$.post(window.location.origin + "/host_setup", setupID);
-    if(debug) console.log(setupID);
-    $("#lobby-num").html("Lobby id: " + gameData.hostID);
-  }
-  else{
-    let joinRequest = path.substring(path.lastIndexOf("/")+5,path.length); //+5 for "join" offset
-    socket.emit("join_lobby", {join: joinRequest, player: gameData.playerID});
-    if(debug) console.log(joinRequest);
-    gameData.hostID = joinRequest;
-    $("#lobby-num").html("Lobby id: " + gameData.hostID);
-  }
-}
-
-//client side AJAX is needed for post method without reloading/redirecting
-$(() => {
-  $("#setup-btn").click(function () {
-    //clear any existing game data
-    clearGameData();
-    gameData.size.rows = $("#rows").val();
-    gameData.size.cols = $("#cols").val();
-    for (let i = 0; i < $("#playerCount").val(); i++) {
-      gameData.players.push(generatePlayers(i + 1));
-    }
-    gameData.targetScore = $("#targetScore").val();
-    gameData.boardState = drawBoard(gameData.size);
-    $("#turnInd").html("Player 1 start");
-    $("#scoreDisplay").html("");
-
-    $.post(window.location.origin + "/setup", gameData);
-  });
-});
-
-//check where mouse click is on canvas (scaled for css size changes)
-canvas.addEventListener("mousedown", function (e) {
-  let myTurn = false;
-  //check if the turn id matches with this players id
-  if(gameData.playerID === gameData.players[gameData.currentPlayerID-1].uuid){
-    myTurn = true;
-  }
-  if(myTurn){
-    //check click position on canvas
-    let t = getMousePos(canvas, e);
-    //validate move, update board array data and calc score
-    let s = boardClick(t, gameData.boardState, gameData.currentPlayerID, debug);
-    if(debug)console.log("click for: ");
-    if(debug)console.log(s);
-    gameData.players[gameData.currentPlayerID - 1].score += s.score;
-
-    //move to next turn
-    if (s.updated) gameData.currentPlayerID++;
-    if (gameData.currentPlayerID > gameData.players.length) {
-      gameData.currentPlayerID = 1;
-    }
-
-    //calls update functions through socket
-    if (s.updated) {
-      $.post(window.location.origin + "/update", gameData);
-    }
-  }
-  else{
-    if(debug) console.log("not your turn");
-  }
-
-});
-
+//perform same function as the setup button based on server socket pushing data
+//as well as updating input fields to match for consistency
+//params: data - matches gameData structure, which is passed from the originating client to server
 function remoteSetupGame(data) {
   clearGameData();
   gameData.size.rows = parseInt(data.size.rows);
@@ -161,7 +59,11 @@ function remoteSetupGame(data) {
   $("#targetScore").val(gameData.targetScore);
 }
 
+//takes gameData passed from another client through the server and updates the display
+//show scores, fill in the grid, check for end game and winners
+//params: data matches the gameData structure as sent from the server via a client
 function remoteUpdateGame(data) {
+  //server converts numbers to strings, need to make sure data is stored properly
   gameData.size = data.size;
   for (let x = 0; x < gameData.boardState.length; x++) {
     for (let y = 0; y < gameData.boardState[0].length; y++) {
@@ -188,6 +90,136 @@ function remoteUpdateGame(data) {
   );
 }
 
+//check if a player is joining as host or to an existing lobby
+//emit socket events for the server to correctly allocate rooms for future socket communication
+//params: ids {host: , player: } - IDs that are assigned when a socket connection is established
+function handleConnection(ids) {
+  if (debug) console.log(ids);
+  gameData.hostID = ids.host;
+  gameData.playerID = ids.player;
+
+  //url is lobby/host or lobby/join:gameID depending on previous selection
+  let path = window.location.pathname;
+  if (path.includes("host")) {
+    let setupID = { host: gameData.hostID, player: gameData.playerID };
+    socket.emit("host_setup", setupID);
+    if (debug) console.log(setupID);
+    $("#lobby-num").html("Lobby id: " + gameData.hostID);
+  } else {
+    let joinRequest = path.substring(path.lastIndexOf("/") + 5, path.length); //+5 for "join" offset
+    socket.emit("join_lobby", { join: joinRequest, player: gameData.playerID });
+    if (debug) console.log(joinRequest);
+    gameData.hostID = joinRequest;
+    $("#lobby-num").html("Lobby id: " + gameData.hostID);
+  }
+}
+
+//assign uuids to the local game data player objects and relate to local ids for turn order
+//can only called after remote/local setup functions to populate the gameData.players array
+//params: idArr[] - an array of UUIDs for the room that this socket is connected to
+//account for players in lobby being either more of less than players in the game
+//for more in lobby - assign in order of joining
+//for more in game - repeat assigns
+function updatePlayers(idArr) {
+  if (debug) console.log(idArr);
+  gameData.inLobby = idArr.length;
+  let thisPlayer = "";
+  if (gameData.players.length > 0) {
+    //account for players in lobby being either more of less than players in the game
+    let k = 0;
+    for (let i = 0; i < gameData.players.length; i++) {
+      gameData.players[i].uuid = idArr[k];
+      k++;
+      if (k >= idArr.length) k = 0;
+      //the uuid matches this clients ID so assign player
+      if (gameData.players[i].uuid === gameData.playerID) {
+        if (thisPlayer.length >= 1) thisPlayer += ",";
+        thisPlayer += gameData.players[i].id;
+      }
+    }
+  }
+  $("#player-num").html("Player: " + thisPlayer);
+}
+
+//set the current player count to the value sent from the server
+function playerJoined(count) {
+  gameData.inLobby = count;
+  $("#player-count").html(gameData.inLobby + " players in lobby");
+}
+
+//decrement the current player count when a socket disconnects
+function playerLeft() {
+  gameData.inLobby--;
+  $("#player-count").html(gameData.inLobby + " players in lobby");
+}
+
+//send to homepage
+function sendHome() {
+  window.location.href = window.location.origin;
+}
+
+/********************************************************************/
+/********************event processing functions**********************/
+/********************************************************************/
+
+//client script so the setup button won't redirect/reload the page
+//setup the canvas for play, based on the input boxes
+$(() => {
+  $("#setup-btn").click(function () {
+    //clear any existing game data
+    clearGameData();
+    gameData.size.rows = $("#rows").val();
+    gameData.size.cols = $("#cols").val();
+    for (let i = 0; i < $("#playerCount").val(); i++) {
+      gameData.players.push(generatePlayers(i + 1));
+    }
+    gameData.targetScore = $("#targetScore").val();
+    gameData.boardState = drawBoard(gameData.size);
+    $("#turnInd").html("Player 1 start");
+    $("#scoreDisplay").html("");
+    //post to the server so that it can re-emit to all the other sockets in the same room and update them as well
+    //client side sockets don't have rooms
+    $.post(window.location.origin + "/setup", gameData);
+  });
+});
+
+//listener to check where a mouse click is on canvas (scaled for css size changes)
+//check turn vs client id and allow for game action to occur
+//update game state based on click and send to server to update other connected sockets
+canvas.addEventListener("mousedown", function (e) {
+  let myTurn = false;
+  //check if the turn id matches with this players id
+  if (
+    gameData.playerID === gameData.players[gameData.currentPlayerID - 1].uuid
+  ) {
+    myTurn = true;
+  }
+  if (myTurn) {
+    //check click position on canvas
+    let t = getMousePos(canvas, e);
+    //validate move, update board array data and calc score
+    let s = boardClick(t, gameData.boardState, gameData.currentPlayerID, debug);
+    if (debug) console.log("click for: ");
+    if (debug) console.log(s);
+    gameData.players[gameData.currentPlayerID - 1].score += s.score;
+
+    //move to next turn
+    if (s.updated) gameData.currentPlayerID++;
+    if (gameData.currentPlayerID > gameData.players.length) {
+      gameData.currentPlayerID = 1;
+    }
+
+    //calls update functions through socket
+    //post to the server so that it can re-emit to all sockets in this room and update them as well (including this one)
+    //client side sockets don't have rooms
+    if (s.updated) {
+      $.post(window.location.origin + "/update", gameData);
+    }
+  } else {
+    if (debug) console.log("not your turn");
+  }
+});
+
 //return mouse position on a canvas from a mousedown event listener - scaled for css size changes
 //0,0 is top left of canvas. Does not handle any rotation or translation of co-ord system
 function getMousePos(canvas, evt, verbose = false) {
@@ -201,6 +233,10 @@ function getMousePos(canvas, evt, verbose = false) {
   if (verbose) console.log(coord.x + "," + coord.y);
   return coord;
 }
+
+/********************************************************************/
+/*********************game play/setup functions**********************/
+/********************************************************************/
 
 //draw grid lines on the canvas
 //return array of 0s for the size passed in
@@ -448,30 +484,8 @@ function scoreMove(playerID, move, board, verbose = false) {
   return addToScore;
 }
 
-function generatePlayers(localID) {
-  let colours = [
-    "#F30E0E",
-    "#f3db0e",
-    "#88F30E",
-    "#0EF395",
-    "#0E84F3",
-    "#C80EF3",
-    "#F30E94",
-    "#0E1D45",
-    "#7CAC88",
-    "#FFFFFF",
-  ];
-  return new Player("", localID, colours[localID - 1], 0);
-}
-
-function clearGameData() {
-  gameData.size = { rows: 5, cols: 5 };
-  gameData.boardState.length = 0;
-  gameData.currentPlayerID = 1;
-  gameData.players.length = 0;
-  gameData.targetScore = 100;
-}
-
+//update HTML to display scores
+//params: players - array of player objects, playerID - current players turn
 function displayScores(players, playerID) {
   let turnStr = "Player " + playerID + "'s turn ";
   let scoreStr = "";
@@ -483,6 +497,12 @@ function displayScores(players, playerID) {
   $("#turnInd").html(turnStr);
 }
 
+//check if the board has any empty spaces, if not check scores against the target and determine closest player
+//ties only show the first player that tied and indicate winning score
+//params:
+//boardState[rows][cols] - array of board state 0 or player id
+//players - array of player objects with IDs and scores
+//targetScore - value that scores will be assessed against
 function displayWinner(boardState, players, targetScore, verbose = false) {
   if (verbose) console.log(boardState);
   let emptyCount = 0;
@@ -509,6 +529,7 @@ function displayWinner(boardState, players, targetScore, verbose = false) {
   }
 }
 
+//part of display winner - when a complete board is found, perform the score calculations
 function checkWinner(playerArr, targetScore, verbose = false) {
   if (verbose) console.log(playerArr);
   if (verbose) console.log(targetScore);
@@ -535,27 +556,35 @@ function checkWinner(playerArr, targetScore, verbose = false) {
   };
 }
 
-function testFill() {
-  gameData.size = { rows: 6, cols: 6 };
-  gameData.boardState = drawBoard(gameData.size, debug);
-  if (debug) console.log(gameData.boardState);
-  // gameData.boardState = [
-  //   [1, 2, 0, 2, 0, 0],
-  //   [0, 0, 0, 1, 0, 0],
-  //   [0, 0, 0, 2, 0, 0],
-  //   [0, 2, 2, 0, 2, 0],
-  //   [0, 1, 0, 2, 0, 0],
-  //   [2, 0, 0, 2, 0, 1],
-  // ];
-  let p1 = new Player("p1-uuid", 1, "#229284", 0);
-  let p2 = new Player("p2-uuid", 2, "#499222", 0);
-  gameData.players.push(p1);
-  gameData.players.push(p2);
-  //fillBoard(gameData.boardState, gameData.players);
+/********************************************************************/
+/***********************convenience functions************************/
+/********************************************************************/
 
-  // let move = {row: 3, col: 5};
-  // let player = 1;
-  // scoreMove(player, move, board);
+//convenience function to create player object templates with a set colour
+//only 10 colours are setup for up to 10 players - doesn't support beyond that
+//params: localID is the player/turn number that is assigned to the object
+function generatePlayers(localID) {
+  let colours = [
+    "#F30E0E",
+    "#f3db0e",
+    "#88F30E",
+    "#0EF395",
+    "#0E84F3",
+    "#C80EF3",
+    "#F30E94",
+    "#0E1D45",
+    "#7CAC88",
+    "#FFFFFF",
+  ];
+  return new Player("", localID, colours[localID - 1], 0);
 }
 
-//testFill();
+//reset the script level gameData object to a default state
+//only changes game variables for clearing games - not network parameters like ID that are assigned on connection
+function clearGameData() {
+  gameData.size = { rows: 5, cols: 5 };
+  gameData.boardState.length = 0;
+  gameData.currentPlayerID = 1;
+  gameData.players.length = 0;
+  gameData.targetScore = 100;
+}
